54c0aed96fc8c1d144884118d4bf840ad3778be5
Eric Wang <twang173@ucsc.edu>
2024-10-15 14:06:47 -0700

    stage 1 complete

--- src/main.ts
+++ src/main.ts
@@ -9,4 +9,17 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-app.insertAdjacentHTML("afterbegin", "🍪");
+const buttonContainer = document.getElementById("button-container");
+
+const button = document.createElement("button");
+button.innerHTML = "🍪"
+
+button.style.padding = "100px, 1000px";
+button.style.fontSize = "50px";
+button.style.cursor = "pointer";
+
+button.addEventListener("click", ()=> {alert('placeholder')});
+
+buttonContainer?.append(button);
+
+
