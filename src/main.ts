16c2aef2cbcb582d4286b60f71e7959ba8af9b51
Eric Wang <twang173@ucsc.edu>
2024-10-15 14:25:48 -0700

    stage 3 complete (tweaked code)

--- src/main.ts
+++ src/main.ts
@@ -19,19 +19,25 @@ button.style.fontSize = "50px";
 button.style.cursor = "pointer";
 
 let clickcount = 0;
+let hasClicked = false;
 
 const countUpdate = () => {
   counter!.innerHTML = `${clickcount} cookie(s)`;
 };
 
-setInterval(() => {
-  clickcount++;
-  countUpdate();
-}, 1000);
 
 button.addEventListener("click", () => {
   clickcount++;
   countUpdate();
+
+  if(!hasClicked){
+    hasClicked = true;
+
+    setInterval(() => {
+        clickcount++;
+        countUpdate();
+      }, 1000);      
+  }
 });
 
 buttonContainer?.append(button);
