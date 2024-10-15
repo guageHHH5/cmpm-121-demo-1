6ef2623ed03ac5bddbe233c7d195a782b7693b7b
Eric Wang <twang173@ucsc.edu>
2024-10-15 14:14:30 -0700

    step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ header.innerHTML = gameName;
 app.append(header);
 
 const buttonContainer = document.getElementById("button-container");
-
+const counter = document.getElementById("counter");
 const button = document.createElement("button");
 button.innerHTML = "🍪";
 
@@ -18,8 +18,11 @@ button.style.padding = "100px, 1000px";
 button.style.fontSize = "50px";
 button.style.cursor = "pointer";
 
+let clickcount = 0;
+
 button.addEventListener("click", () => {
-  alert("placeholder");
+    clickcount++;
+    counter!.innerHTML = `${clickcount} cookie(s)`;
 });
 
 buttonContainer?.append(button);
