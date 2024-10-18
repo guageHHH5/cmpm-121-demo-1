a4640f6931e46f8c4e76d42c03c79d0cc1dd26c5
Eric Wang <twang173@ucsc.edu>
2024-10-18 13:36:53 -0700

    stage 6 complete

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,6 @@ const purchases = document.getElementById("purchases");
 const button = document.createElement("button");
 button.innerHTML = "🍪";
 
-
 button.style.padding = "100px, 1000px";
 button.style.fontSize = "50px";
 button.style.cursor = "pointer";
@@ -29,15 +28,15 @@ let totalgrowth = 0;
 const upgrades = [
   { name: "A", cost: 10, growthRate: 0.1, count: 0 },
   { name: "B", cost: 100, growthRate: 2.0, count: 0 },
-  { name: "C", cost: 1000, growthRate: 50.0, count: 0 }
+  { name: "C", cost: 1000, growthRate: 50.0, count: 0 },
 ];
 
 const countUpdate = () => {
   counter!.innerHTML = `${clickcount.toFixed(0)} cookie(s)`;
-  growthrate!.innerHTML = `Growth Rate: ${totalgrowth.toFixed(2)} cookies/sec`
+  growthrate!.innerHTML = `Growth Rate: ${totalgrowth.toFixed(2)} cookies/sec`;
   purchases!.innerHTML = ` Purchases:
     <ul>
-      ${upgrades.map(upgrade => `<li>${upgrade.name}: ${upgrade.count}</li>`).join('')}
+      ${upgrades.map((upgrade) => `<li>${upgrade.name}: ${upgrade.count}</li>`).join("")}
     </ul>
   `;
 };
@@ -61,32 +60,36 @@ button.addEventListener("click", () => {
   countUpdate();
 });
 
-const handlepurchase = (upgrade: {name : string; cost : number; growthRate : number; count : number}, button : HTMLButtonElement)=>{
-  if(clickcount >= upgrade.cost){
+const handlepurchase = (
+  upgrade: { name: string; cost: number; growthRate: number; count: number },
+  button: HTMLButtonElement,
+) => {
+  if (clickcount >= upgrade.cost) {
     clickcount -= upgrade.cost;
     upgrade.count += 1;
     totalgrowth += upgrade.growthRate;
     countUpdate();
     button.disabled = clickcount < upgrade.cost;
   }
-}
+};
 
-upgrades.forEach(upgrade => {
+upgrades.forEach((upgrade) => {
   const upgradeButton = document.createElement("button");
   upgradeButton.innerHTML = `Buy ${upgrade.name} (${upgrade.cost} 🍪, +${upgrade.growthRate} cookies/sec)`;
   upgradeButton.disabled = true;
 
-  upgradeButton.addEventListener("click", ()=> handlepurchase(upgrade, upgradeButton));
+  upgradeButton.addEventListener("click", () =>
+    handlepurchase(upgrade, upgradeButton),
+  );
   upgradeContainer?.append(upgradeButton);
 
-  const checkUpgrade = ()=>{
+  const checkUpgrade = () => {
     upgradeButton.disabled = clickcount < upgrade.cost;
     requestAnimationFrame(checkUpgrade);
   };
   checkUpgrade();
-})
+});
 
 buttonContainer?.append(button);
-
 
 requestAnimationFrame(animatedIncrement);
